f1bce3e3446712b9cc0e2b75378cf90d1ddbf85d
Azabell1993 <moreirly@naver.com>
2022-01-29 21:49:54 +0900

    props and send to object

--- src/App.js
+++ src/App.js
@@ -3,26 +3,41 @@ import './App.css';
 // import { Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+
+  function myFunc() {
+    console.log("This is function.")
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={ <Component1 /> } />
-        <Route path="/jiwoo" element={ <Component2 /> } />
+        <Route exact path="/" element={ <Component1 additionalText="이 바보야 진짜 아니야" func={ myFunc } /> } />
+        <Route path="/jiwoo" element={ <Component2  additionalText="이 바보야 진짜 아니야" func={ myFunc } /> } />
         <Route element={ <PageNotFound /> } />
       </Routes>
     </BrowserRouter>
   );
 }
 
-function Component1() {
+function Component1({additionalText, func}) {
+
+  func()
+
   return (
-    <div>This is Component1 at root context path.</div>
+    <div>
+      <p>This is Component1 at root context path.</p>
+      <p>And the additional test is: {additionalText}</p>
+    </div>
   )
 }
 
-function Component2() {
+function Component2( obj ) {
   return (
-    <div>This is Component2 at "jiwoo" path.</div>
+    <div>
+      <p>This is Component2 at "jiwoo" path.</p>
+      <p>And the additional test is: { obj.additionalText }</p>
+      <button onClick={ obj.func }>Click here and ook at the console.</button>
+    </div>
   )
 
 }
